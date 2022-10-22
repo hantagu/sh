373fb52f52324305ce26e381b30a9af9efdd9c5a
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2022-10-22 15:40:45 +0300

    Create 0.sh

---
+++ 0.sh
@@ -0,0 +1,10 @@
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf -y install gnome-shell gnome-backgrounds gnome-terminal gnome-terminal-nautilus nautilus gnome-text-editor evince eog file-roller gnome-disk-utility gnome-tweaks firefox
+sudo dnf -y install bash-completion ffmpeg cups wireguard-tools
+
+sudo systemctl set-default graphical.target
+
+sudo systemctl stop firewalld.service
+sudo systemctl disable firewalld.service
