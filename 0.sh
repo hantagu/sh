8d784c8acf8ca18b9b0ef2b8e9901eade79290e2
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2022-12-11 02:39:49 +0300

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -2,7 +2,7 @@ sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf -y install gnome-shell gnome-backgrounds gnome-terminal gnome-terminal-nautilus nautilus gnome-text-editor evince eog file-roller gnome-disk-utility gnome-tweaks firefox
-sudo dnf -y install bash-completion ffmpeg cups wireguard-tools
+sudo dnf -y install bash-completion ffmpeg cups wireguard-tools fuse flatpak
 
 sudo systemctl set-default graphical.target
 
