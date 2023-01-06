f648db6b8b5dc7d4d5f3adb481b15025fc37798d
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-01-07 00:52:39 +0300

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,21 @@
+HISTFILE=/dev/null
+
+if [[ ! -f $PREFIX/share/bash-completion/bash_completion ]]
+then
+	yes | pkg update
+	termux-setup-package-manager
+	termux-change-repo
+	yes | pkg upgrade
+	yes | pkg install man manpages gnupg openssl-tool openssh micro
+	yes | pkg install clang python
+	ln -s /storage/emulated/0 storage
+fi
+
+alias c="clear"
+alias rm="rm -rf"
+alias ls="ls --group-directories-first"
+alias ll="ls --group-directories-first -A"
+alias ps="ps -eHo comm,pid,ppid,pgid,sid,tty"
+
+alias m="micro"
+alias p="python3"
