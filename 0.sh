5ccad630ef2d805558981bc45b7bbde887dd9663
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-01-26 19:41:53 +0300

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -2,9 +2,11 @@ sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf -y install gnome-shell gnome-backgrounds gnome-terminal gnome-terminal-nautilus nautilus gnome-text-editor evince eog file-roller gnome-disk-utility gnome-tweaks firefox
-sudo dnf -y install bash-completion ffmpeg cups wireguard-tools fuse flatpak
+sudo dnf -y install bash-completion docker git ffmpeg cups wireguard-tools fuse flatpak
+
+sudo usermod -aG docker $USER
 
 sudo systemctl set-default graphical.target
 
-sudo systemctl stop firewalld.service
-sudo systemctl disable firewalld.service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
