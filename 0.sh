9ef5b6f8b08e7ce84248e2820b4c148416d7f785
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-01-26 20:16:44 +0300

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -4,9 +4,12 @@ sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfr
 sudo dnf -y install gnome-shell gnome-backgrounds gnome-terminal gnome-terminal-nautilus nautilus gnome-text-editor evince eog file-roller gnome-disk-utility gnome-tweaks firefox
 sudo dnf -y install bash-completion docker git ffmpeg cups wireguard-tools fuse flatpak
 
-sudo usermod -aG docker $USER
+curl --output "tmp.zip" -sL https://fonts.google.com/download?family=Ubuntu && unzip -qq -u -d ~/.local/share/fonts "tmp.zip" && rm "tmp.zip"
+curl --output "tmp.zip" -sL https://fonts.google.com/download?family=Ubuntu%20Mono && unzip -qq -u -d ~/.local/share/fonts "tmp.zip" && rm "tmp.zip"
 
-sudo systemctl set-default graphical.target
+sudo usermod -aG docker $USER
 
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+
+sudo systemctl set-default graphical.target
