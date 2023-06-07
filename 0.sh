75f99524863f160d17be2f9743a6d604fa7c9b74
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-06-08 00:06:12 +0300

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -4,17 +4,10 @@ sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfr
 sudo dnf -y install gnome-shell gnome-backgrounds gnome-console nautilus gnome-text-editor evince eog file-roller gnome-disk-utility gnome-tweaks firefox
 sudo dnf -y install bash-completion docker git ffmpeg cups wireguard-tools fuse flatpak
 
-mkdir -p ~/.local/share/fonts
-curl -sL https://raw.githubusercontent.com/hantagu/sh/main/Ubuntu.tar.xz | tar -C ~/.local/share/fonts -Jx
-
 sudo usermod -aG docker $USER
 
-read -p "1/2: " x
-if [ $x = "1" ]; then
-    sudo dnf install xorg-x11-drv-nvidia-cuda
-elif [ $x = "2" ]; then
-    sudo dnf install @hardware-support
-fi
+mkdir -p ~/.local/share/fonts
+curl -sL https://raw.githubusercontent.com/hantagu/sh/main/Ubuntu.tar.xz | tar -C ~/.local/share/fonts -Jx
 
 sudo systemctl disable avahi-daemon.socket avahi-daemon.service
 sudo systemctl stop avahi-daemon.socket avahi-daemon.service
