45567b563622ef551fdc778082a794a82a2d2a7e
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-11-26 01:17:22 +0300

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -2,7 +2,7 @@ sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf -y install gnome-shell gnome-backgrounds gnome-console nautilus gnome-text-editor evince loupe totem file-roller gnome-disk-utility gnome-software gnome-tweaks firefox
-sudo dnf -y install fuse flatpak ffmpeg bash-completion git cups wireguard-tools 
+sudo dnf -y install tar fuse flatpak ffmpeg bash-completion git cups wireguard-tools 
 
 mkdir -p ~/.local/share/fonts
 curl -sL https://raw.githubusercontent.com/hantagu/sh/main/ubuntu-font-family-0.83.tar.xz | tar -C ~/.local/share/fonts -Jx
