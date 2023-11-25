d9186bc316f3e3999ed4a7fab6d8375c06cf2642
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-11-25 17:59:40 +0300

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -1,7 +1,7 @@
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf -y install gnome-shell gnome-backgrounds gnome-console nautilus gnome-text-editor evince loupe totem file-roller gnome-disk-utility gnome-tweaks firefox
+sudo dnf -y install gnome-shell gnome-backgrounds gnome-console nautilus gnome-text-editor evince loupe totem file-roller gnome-disk-utility gnome-software gnome-tweaks firefox
 sudo dnf -y install fuse flatpak ffmpeg bash-completion git cups wireguard-tools 
 
 mkdir -p ~/.local/share/fonts
