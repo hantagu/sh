6d9d58ed9c4e8cff1afc12519f25bf48636a6781
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-12-18 22:45:25 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,15 +1,13 @@
 for i in ~/.local/bin/*; do
-	if [ ! -d $i ]; then
-		continue
-	fi
-
-	if [ -d $i/bin ]; then
-		PATH=$i/bin:$PATH
-	else
-		PATH=$i:$PATH
-	fi
+    if [ -d $i/bin ]; then
+        PATH=$i/bin:$PATH
+    elif [ -d $i ]; then
+        PATH=$i:$PATH
+    fi
 done
 
+PATH=~/.local/bin:$PATH
+
 if [ -f /etc/bashrc ]; then
-	source /etc/bashrc
+    source /etc/bashrc
 fi
